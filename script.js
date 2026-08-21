cfd2163839667a13ac2030ba176f88238d93bc95
Ricardo <ricardo80@gmail.com>
2026-08-21 16:53:48 -0300

    identação de código

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const toggle=document.querySelector('.menu-toggle'),menu=document.querySelector('.menu');
-toggle?.addEventListener('click',()=>{menu.classList.toggle('open')});
-document.querySelectorAll('.menu a').forEach(a=>a.addEventListener('click',()=>menu.classList.remove('open')));
+const toggle = document.querySelector('.menu-toggle'), menu = document.querySelector('.menu');
+toggle?.addEventListener('click', () => { menu.classList.toggle('open') });
+document.querySelectorAll('.menu a').forEach(a => a.addEventListener('click', () => menu.classList.remove('open')));
